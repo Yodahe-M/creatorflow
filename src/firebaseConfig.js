7e4b880e13aa4852ad565a254c3db1f2ebd0a21e
Yodahe-M <104659062+Yodahe-M@users.noreply.github.com>
2025-07-30 11:39:58 -0600

    Add new JS file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1GI469Bl6iG-uQfruA4Wbm7WTtoWWOAM",
+  authDomain: "creatorflow-ad62f.firebaseapp.com",
+  projectId: "creatorflow-ad62f",
+  storageBucket: "creatorflow-ad62f.firebasestorage.app",
+  messagingSenderId: "996210401929",
+  appId: "1:996210401929:web:da3019b805d9eff45d1c58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
